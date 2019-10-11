aadc431a77d132db6909d3af4b4d6d56daf1d238
charl-alexandre le brun <charl-alexandre@ramoreik.com>
2019-10-11 02:56:55 -0400

    added some todos

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.classes.themer import Themer
 import argparse
 
+# TODO: Remake install scripts to be more adequate and modern, also to include more technologies for arch and ubuntu
+# TODO: Add templates with an additionnal layer of abstraction, like main color, accents, etc. 
+        # The goal is to make customization very simple for everynone
+
 def handle_args():
     parser = argparse.ArgumentParser(description='EZThemer made by Ramoreik')
     parser.add_argument('-t','--theme', type=str, help='Load Appearance theme from yaml file', required=True)
