b7892edbb172c9a154b4b200b1128fe3e83c9706
charl-alexandre le brun <charl-alexandre@ramoreik.com>
2019-10-14 15:00:48 -0400

    added pcsx2 as floating and todos

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from src.classes.themer import Themer
 import argparse
 
 # TODO: Remake install scripts to be more adequate and modern, also to include more technologies for arch and ubuntu
-# TODO: Add templates with an additionnal layer of abstraction, like main color, accents, etc. 
-        # The goal is to make customization very simple for everynone
+# TODO: Add templates with an additionnal layer of abstraction, like main color, accents, etc.
+        # The goal is to make customization very simple for everyone
+        # Make simplified version of templates and a new "simple theme" template
 
 def handle_args():
     parser = argparse.ArgumentParser(description='EZThemer made by Ramoreik')
